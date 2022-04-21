6c8a6083d13f5a3310fdc92d5941339f315e2344
darshanhegde27 <darshanhegdesonda@gmail.com>
2022-04-21 18:44:07 +0530

    3 files modified, 3 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './style.css';
-
+import store from './Redux/Store/store';
+import { Provider } from 'react-redux';
 export default function App() {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 }
--- src/Views/components/Home.jsx
+++ src/Views/components/Home.jsx
@@ -2,16 +2,13 @@ import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import React from 'react';
 
-export default class Dash extends React.Component {
+export default class Home extends React.Component {
   state = {
     loading: true,
     Data: [],
   };
-   componentDidMount() {
-  
-  }
+  componentDidMount() {}
   render() {
-    
     return (
       <div>
         {this.state.loading ? <div>loading</div> : console.log(this.state.Data)}
